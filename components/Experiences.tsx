77485e786f1eddeb273472f4d91e79c4ca9fc54f
Nur Harizul Aiman Bin Mat Harith <harizulaiman@gmail.com>
2024-07-14 17:34:25 +0800

    fix experiences

--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -16,13 +16,13 @@ function Experiences() {
                 className="flex-1 text-white border-neutral-200 dark:border-slate-800"
                 duration={Math.floor(Math.random() * 10000) + 10000}
             >
-                <div className="flex lg:flex-row flex-col w-full h-full lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
+                <div className="flex lg:flex-row flex-col w-full h-full items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
                     <img src={thumbnail} alt={thumbnail} className="lg:w-32 md:w-20 w-16" />
                     <div className="lg:ms-5">
-                        <h1 className="text-start text-xl md:text-2xl font-bold">
+                        <h1 className="text-center lg:text-start text-xl md:text-2xl font-bold">
                             {title}
                         </h1>
-                        <p className="text-start text-white-100 mt-3 font-semibold">
+                        <p className="text-center lg:text-start text-white-100 mt-3 font-semibold">
                             {desc}
                         </p>
                     </div>
