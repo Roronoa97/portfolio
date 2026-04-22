a706bff26aaed6caca0a001752758490180b9e03
roronoa97 <harizulaiman@gmail.com>
2026-04-22 19:15:29 +0800

    fix for production

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,8 @@
 import type { Metadata } from "next";
-import { Inter, Geist } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/sonner";
-
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Joy Portfolio",
@@ -21,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className={cn("font-sans", geist.variable)}>
-      <body className={inter.className}>
+    <html lang="en" suppressHydrationWarning className="font-sans">
+      <body>
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
